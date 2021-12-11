b08470bf3e35736364240f23dcaa34dc2b078a5e
Ngô Nhật Tân <56728366+NhatTanNgo@users.noreply.github.com>
2021-12-11 22:59:03 +0700

    FindOrderByUserID

---
+++ src/main/java/com/example/demo/DonHang/ProductEntity.java
@@ -0,0 +1,22 @@
+package com.example.demo.DonHang;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class ProductEntity {
+
+    private String product_ID;
+    private  int quantity;
+    private float unitPrice;
+}
